56c594265c70355a5571aa8aedf5d1f232f6328e
tientran97 <tien.minhtran615034@gmail.com>
2023-10-31 21:40:17 +0700

    Adding Reactjs, Redux, Router, Tailwind

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,17 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+"""
+    ^.*
+    ^ asserts position at start of a line
+    . matches any character (except for line terminators)
+    * matches the previous token between zero and unlimited times, as many times as possible, giving back as needed (greedy)
+"""
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
